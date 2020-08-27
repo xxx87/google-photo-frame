f79eaf97c1c9f3e7194c899d3d2c6987743c1916
i.isaev <i.isaev@pronet.ua>
2020-08-27 09:50:37 +0300

    CR

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-"use strict";
-
-// [START app]
 require("dotenv").config();
 const async = require("async");
 const bodyParser = require("body-parser");
@@ -28,7 +11,7 @@ const session = require("express-session");
 const sessionFileStore = require("session-file-store");
 const uuid = require("uuid");
 const winston = require("winston");
-
+const passport = require("passport");
 const app = express();
 const fileStore = sessionFileStore(session);
 const server = http.Server(app);
@@ -56,7 +39,6 @@ const storage = persist.create({ dir: "persist-storage/" });
 storage.init();
 
 // Настройте аутентификацию OAuth 2.0 через библиотеку Passport.js.
-const passport = require("passport");
 const auth = require("./auth");
 auth(passport);
 
--- config.js
+++ config.js
@@ -16,32 +16,23 @@
 
 const config = {};
 
-// The OAuth client ID from the Google Developers console.
+// The OAuth client ID and secret from the Google Developers console.
 config.oAuthClientID = process.env.AUTH_CLIENT_ID;
-
-// The OAuth client secret from the Google Developers console.
 config.oAuthclientSecret = process.env.AUTH_CLIENT_SECRET;
-
 // The callback to use for OAuth requests. This is the URL where the app is
 // running. For testing and running it locally, use 127.0.0.1.
 config.oAuthCallbackUrl = process.env.AUTH_CALLBACK_URL;
-
 // The port where the app should listen for requests.
 config.port = 8080;
-
 // The scopes to request. The app requires the photoslibrary.readonly and
 // plus.me scopes.
 config.scopes = ["https://www.googleapis.com/auth/photoslibrary.readonly", "profile"];
-
 // The number of photos to load for search requests.
 config.photosToLoad = 150;
-
 // The page size to use for search requests. 100 is reccommended.
 config.searchPageSize = 100;
-
 // The page size to use for the listing albums request. 50 is reccommended.
 config.albumPageSize = 50;
-
 // The API end point to use. Do not change.
 config.apiEndpoint = "https://photoslibrary.googleapis.com";
 
